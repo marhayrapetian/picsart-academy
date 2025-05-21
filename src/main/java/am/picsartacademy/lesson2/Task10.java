5410fa9e2309f0be7b3aa5e9e18bccf365bdee5b
mariamhayrapetyan <marhayrapetiann@gmail.com>
2025-05-21 21:44:43 +0400

    add Homework3

---
+++ src/main/java/am/picsartacademy/lesson2/Task10.java
@@ -0,0 +1,20 @@
+package am.picsartacademy.lesson2;
+
+public class Task10 {
+    public static void main(String[] args) {
+        int N = 5;
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+
